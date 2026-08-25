241a5dc9f3c58c94381544423310081bf6340f61
Joshua PZ <joshuaprieto8@gmail.com>
2026-08-24 20:21:41 -0500

    feat: SDK configurator and Credentials value object
    Add a Credentials value-object (publicKey, privateKey) and a SdkConfigurator skeleton. Files added:
    - sdk/src/domain/value-objects/Credentials.ts: exports Credentials interface.
    - sdk/src/infrastructure/config/SDKConfigurator.ts: exports SDKOptions and SdkConfigurator with configure, getActiveGateway and getCredentials methods (currently unimplemented and throwing errors). Imports Gateway and Credentials. Intended to centralize SDK configuration; implementation to be provided later.

---
+++ sdk/src/domain/value-objects/Credentials.ts
@@ -0,0 +1,5 @@
+
+export interface Credentials {
+  publicKey: string;
+  privateKey: string;
+}
---
+++ sdk/src/infrastructure/config/SDKConfigurator.ts
@@ -0,0 +1,20 @@
+import { Gateway } from "../../domain/value-objects/Gateway"
+import { Credentials } from "../../domain/value-objects/Credentials"
+
+export interface SDKOptions {
+  gateway: Gateway;
+  credentials: Record<string, Credentials>;
+}
+
+export class SdkConfigurator {
+  configure (options:SDKOptions): void {
+    throw new Error("Aun no esta implementado");
+  }
+  getActiveGateway(): Gateway {
+    throw new Error("aun no esta implementado");
+  }
+
+  getCredentials(gateway: Gateway): Credentials {
+    throw new Error("aun no esta implementado");
+  }
+}
